a00b49d657d6d294ec5fc3c0623583fbabcc74ee
unknown <lautarorangilt@gmail.com>
2021-09-21 19:47:02 -0300

    getcharacters

--- api/src/controllers/characterController.js
+++ api/src/controllers/characterController.js
@@ -23,20 +23,39 @@ const addCharacter = (req,res, next)=>{
 }
 
 async function getCharacters(req, res, next){
-    try {
+    
 
         let apiCharacters = (await axios.get("https://rickandmortyapi.com/api/character")).data.results
         let dbCharacters= await Characters.findAll({include: Episodes})
-        let allChars= dbCharacters.concat(apiCharacters)
-
-
-
-
-        res.send(allChars)
-
-    } catch (error) {
-        next(error)
-    }
+        
+        return Promise.all([
+            apiCharacters,
+            dbCharacters
+        ]).then(resultados => {
+            var apiCharacters = resultados[0].data.results
+            var dbCharacters = resultados[1]
+    
+    
+    
+            apiCharacters = apiCharacters.map((character) => {
+                return {
+                    id: character.id,
+                    name: character.name,
+                    image: character.image
+                }
+            })
+            dbCharacters = dbCharacters.map((character) =>{
+                return {
+                    id: character.id,
+                    name: character.name,
+                    image: character.image
+                }
+            })
+            var allCharacters = apiCharacters.concat(dbCharacters)
+            res.send(allCharacters)
+        })
+        .catch(error => next(error))
+   
 }
 
 

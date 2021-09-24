cfeeef9777e1ccdcba6752274f3d34c4a95c2e36
unknown <lautarorangilt@gmail.com>
2021-09-23 23:35:37 -0300

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React from "react";
-
+import { Route} from "react-router-dom";
+import LandingPage from "./components/LandingPage.js"
+import Home from "./components/Home.js"
+import NavBar from "./components/NavBar.js"
 
 
 
 function App() {
   return (
     <div className="App">
+      <Route exact path="/" component={LandingPage}/>
+      <Route exact path="/home" component={Home}/>
+      <Route path="/home" component={NavBar}/>
       
     </div>
   );
